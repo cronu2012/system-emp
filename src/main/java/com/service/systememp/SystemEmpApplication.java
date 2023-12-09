247eacebe0e6e9ddd2c2f0c32e32d84560483f4b
cronu <cronu2012@gmail.com>
2023-12-09 18:08:49 +0800

    init

---
+++ src/main/java/com/service/systememp/SystemEmpApplication.java
@@ -0,0 +1,13 @@
+package com.service.systememp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemEmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemEmpApplication.class, args);
+	}
+
+}
